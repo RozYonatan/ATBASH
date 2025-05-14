fd5a95b98064e05064aa3392fad80fe4cf06674b
hershy rozenfeld <Y6764057@gmail.com>
2025-05-14 15:27:34 +0300

    added a message and key-words to find into the message

--- ATBASH/Program.cs
+++ ATBASH/Program.cs
@@ -10,8 +10,10 @@ namespace ATBASH
     {
         static void Main(string[] args)
         {
-        
+            string message = "Lfi ulixvh ziv kivkzirmt uli z nzqli zggzxp lm gsv Arlmrhg vmvnb. Gsv ilxpvg fmrgh ziv ivzwb zmw dzrgrmt uli gsv hrtmzo. Ylnyh szev yvvm kozxvw mvzi pvb olxzgrlmh. Mfpsyz urtsgvih ziv hgzmwrmt yb uli tilfmw rmurogizgrlm. Gsv zggzxp droo yv hfwwvm zmw hgilmt -- gsvb dlm’g hvv rg xlnrmt.Dv nfhg hgzb srwwvm zmw pvvk gsv kozm hvxivg fmgro gsv ozhg nlnvmg. Erxglib rh mvzi. Hgzb ivzwb.";
+            string[] words = { "bomb", "nukhba", "fighter", "rocket", "secret" };
         }
+
         static int dangerCheck(string[] words, string sentence)
         {
             int danger = 0;
