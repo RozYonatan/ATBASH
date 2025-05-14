20fa2ecd7acef2540219450687dc80a96d826502
Yonatan Rosenfeld <yonatan101200@gmail.com>
2025-05-14 15:29:42 +0300

    descrip function

--- ATBASH/Program.cs
+++ ATBASH/Program.cs
@@ -8,9 +8,34 @@ namespace ATBASH
 {
     internal class Program
     {
+        static int convert(int asciivalu)
+        {
+            int asci2num = 218;
+            return asci2num - asciivalu;
+        }
+        static string description(string message)
+        {
+            char ch;
+            int asciiValu;
+            string descripMessage = "";
+            foreach (char c in message)
+            { 
+                if (char.IsLetter(c))
+                {
+                    ch = char.ToLower(c);
+                    asciiValu = (int)ch;
+                    ch = (char)convert(asciiValu);
+                    descripMessage += ch;
+                }
+                else
+                {
+                    descripMessage += c;
+                }
+            }
+            return descripMessage;
+        }
         static void Main(string[] args)
         {
-        // hi i'm yonatan
         }
     }
 }
