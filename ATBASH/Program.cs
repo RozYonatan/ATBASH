eb0b394c51e31864724570734712499aeb9332cd
hershy rozenfeld <Y6764057@gmail.com>
2025-05-14 15:10:40 +0300

    added function to print a special warning

--- ATBASH/Program.cs
+++ ATBASH/Program.cs
@@ -10,7 +10,7 @@ namespace ATBASH
     {
         static void Main(string[] args)
         {
-        // hi i'm yonatan
+        
         }
         static int dangerCheck(string[] words, string sentence)
         {
@@ -23,6 +23,25 @@ namespace ATBASH
                 }
             }
             return danger;
+        }
+        static void warning(string sentence, int danger)
+        {
+            if (danger >= 1 && danger <= 5)
+            {
+                Console.WriteLine($"WARNING!: {sentence} get {danger} points");
+            }
+            else if (danger >= 6 && danger <= 10)
+            {
+                Console.WriteLine($"DANGER!: {sentence} get {danger} points");
+            }
+            else if (danger >= 11 && danger <= 15)
+            {
+                Console.WriteLine($"ULTRA ALERT!: {sentence} get {danger} points");
+            }
+            else
+            {
+                Console.WriteLine("The sentence is safe.");
+            }
         }
     }
 }
